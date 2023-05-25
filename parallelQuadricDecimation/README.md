b74c47eb5f4fa9f08db10efaa90742947cf3fa51
Louis-Pujol <louispujol@laposte.net>
2023-05-25 11:54:55 +0200

    update python code with new getters interface

--- parallelQuadricDecimation/README.md
+++ parallelQuadricDecimation/README.md
@@ -20,9 +20,9 @@ So far to make it run :
 - Understand deeper the algo : why the return of pyvista.decimate is not in the same order as our manual decimation and imposes us te reorder points ? Does it come from pyVista or from vtk ?
 - Why with a high target reduction there is a mismatch between manual decimation and pyvista.decimate (example with the cow and target_reduction=0.95) ? At first glance it seems to happen when a topological change append, maybe this is good to alert the user and stop decimation before it happens ?
 - we want to make records accessible with a proper getter and not with this ugly technique...
-- how to integrate this slighty modified vtk in scikit-shapes ?
+- how to integrate this slighty modified vtk in scikit-shapes ? -> look at CMake/vtkWheel* to modify the name of the library and avoid potential conflict with true vtk (OK solution but heavy)
 
 ### Python
-- parallel decimation seems ok, but we need to write projection of indices from original to decimated points
+- parallel decimation seems ok, but we need to write projection of indices from original to decimated points : OK
 
 
--- parallelQuadricDecimation/animation.py
+++ parallelQuadricDecimation/animation.py
@@ -5,9 +5,15 @@ import vedo
 
 timer_id = -1
 
-def play_animation(mesh_sequence, framerate=5, radius=0.005,
-                   landmarks=None, mappings=None, radius_landmarks=0.01):
 
+def play_animation(
+    mesh_sequence,
+    framerate=5,
+    radius=0.005,
+    landmarks=None,
+    mappings=None,
+    radius_landmarks=0.01,
+):
     def bfunc():
         global timer_id
         plotter.timer_callback("destroy", timer_id)
@@ -22,28 +28,33 @@ def play_animation(mesh_sequence, framerate=5, radius=0.005,
 
         i = int(t * framerate) % len(mesh_sequence)
 
-        
         if landmarks is None:
-            plotter.pop().pop().pop().add([
-                "Number of points: " + str(len(mesh_sequence[i].points)),
-                vedo.Mesh(mesh_sequence[i], c='k'),
-                vedo.Spheres(mesh_sequence[i].points, r=radius, c='r')
-            ]).render()
+            plotter.pop().pop().pop().add(
+                [
+                    "Number of points: " + str(len(mesh_sequence[i].points)),
+                    vedo.Mesh(mesh_sequence[i], c="k"),
+                    vedo.Spheres(mesh_sequence[i].points, r=radius, c="r"),
+                ]
+            ).render()
 
         else:
-            plotter.pop().pop().pop().pop().add([
-                "Number of points: " + str(len(mesh_sequence[i].points)),
-                vedo.Mesh(mesh_sequence[i], c='k'),
-                vedo.Spheres(mesh_sequence[i].points, r=radius, c='r'),
-                vedo.Spheres(mesh_sequence[i].points[mappings[i][landmarks]], r=radius_landmarks, c='b')
-            ]).render()
-
-
+            plotter.pop().pop().pop().pop().add(
+                [
+                    "Number of points: " + str(len(mesh_sequence[i].points)),
+                    vedo.Mesh(mesh_sequence[i], c="k"),
+                    vedo.Spheres(mesh_sequence[i].points, r=radius, c="r"),
+                    vedo.Spheres(
+                        mesh_sequence[i].points[mappings[i][landmarks]],
+                        r=radius_landmarks,
+                        c="b",
+                    ),
+                ]
+            ).render()
 
     timer_id = -1
     t0 = time.time()
-    plotter= vedo.Plotter(axes=0)
-    button = plotter.add_button(bfunc, states=[" Play ","Pause"], size=40)
+    plotter = vedo.Plotter(axes=0)
+    button = plotter.add_button(bfunc, states=[" Play ", "Pause"], size=40)
     evntId = plotter.add_callback("timer", handle_timer)
 
     npoints = len(mesh_sequence[0].points)
@@ -51,19 +62,23 @@ def play_animation(mesh_sequence, framerate=5, radius=0.005,
     if landmarks is None:
         plotter.show(
             [
-            "Number of points: " + str(npoints),
-            vedo.Mesh(mesh_sequence[0], c='k'),
-            vedo.Spheres(mesh_sequence[0].points, r=radius, c='r')
+                "Number of points: " + str(npoints),
+                vedo.Mesh(mesh_sequence[0], c="k"),
+                vedo.Spheres(mesh_sequence[0].points, r=radius, c="r"),
             ]
         )
 
     else:
         plotter.show(
             [
-            "Number of points: " + str(npoints),
-            vedo.Mesh(mesh_sequence[0], c='k'),
-            vedo.Spheres(mesh_sequence[0].points, r=radius, c='r'),
-            vedo.Spheres(mesh_sequence[0].points[mappings[0][landmarks]], r=radius_landmarks, c='b')
+                "Number of points: " + str(npoints),
+                vedo.Mesh(mesh_sequence[0], c="k"),
+                vedo.Spheres(mesh_sequence[0].points, r=radius, c="r"),
+                vedo.Spheres(
+                    mesh_sequence[0].points[mappings[0][landmarks]],
+                    r=radius_landmarks,
+                    c="b",
+                ),
             ]
         )
 
@@ -75,25 +90,71 @@ datafolder = "/home/louis/Environnements/singularity_homes/keops-full/GitHub/sci
 reference_mesh = pyvista.read(datafolder + "/" + "mesh030.ply")
 
 d = QuadricDecimation(target_reduction=0.99, use_numba=True)
+start = time.time()
 d.fit(reference_mesh)
+end = time.time()
+print("Fitting took " + str(end - start) + " seconds")
 
 mesh = pyvista.read(datafolder + "/" + "mesh007.ply")
 successive_decimations = []
 indices_mappings = []
 
 for stop_at in [
-        0, 1000, 2000, 4000, 5000, 6000, 7000,
-        7500, 8000, 8500,  9000, 9500, 9750,
-        10000, 10250, 10500, 11000, 11125, 11250,
-        11500, 11750, 12000, 12100, 12200, 12300,
-        12400]:
+    0,
+    1000,
+    2000,
+    4000,
+    5000,
+    6000,
+    7000,
+    7500,
+    8000,
+    8500,
+    9000,
+    9500,
+    9750,
+    10000,
+    10250,
+    10500,
+    11000,
+    11125,
+    11250,
+    11500,
+    11750,
+    12000,
+    12100,
+    12200,
+    12300,
+    12400,
+]:
+    start = time.time()
+    decimated_mesh, indice_mapping = d.partial_transform(mesh, stop_at)
+    end = time.time()
+    print(
+        "Decimation to "
+        + str(stop_at)
+        + " points took "
+        + str(end - start)
+        + " seconds"
+    )
+    successive_decimations.append(decimated_mesh)
+    indices_mappings.append(indice_mapping)
 
-        # decimated_mesh, indice_mapping = progressive_decimation(mesh, stop_at, d, use_numba=True)
-        decimated_mesh, indice_mapping = d.partial_transform(mesh, stop_at)
-        successive_decimations.append(decimated_mesh)
-        indices_mappings.append(indice_mapping)
-
-#10 random landmarks
+# 10 random landmarks
 landmarks = np.random.randint(0, len(mesh.points), 10)
 
-play_animation(successive_decimations, framerate=8, radius=0.003, radius_landmarks=0.01, landmarks=landmarks, mappings=indices_mappings)
+play_animation(
+    successive_decimations,
+    framerate=8,
+    radius=0.003,
+    radius_landmarks=0.01,
+    landmarks=landmarks,
+    mappings=indices_mappings,
+)
+
+
+from vedo.applications import Browser
+
+meshes = [vedo.Mesh(mesh).c("k") for mesh in successive_decimations]
+plt = Browser(meshes, resetcam=0, axes=0)  # a vedo.Plotter
+plt.show().close()
